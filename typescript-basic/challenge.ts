f04c401c70a9582f5c99dfac056872d42674a086
Anne-sophie Marchet <annesophiemarchet@gmail.com>
2024-09-25 08:47:03 +0200

    fix comment

--- typescript-basic/challenge.ts
+++ typescript-basic/challenge.ts
@@ -1,21 +1,24 @@
 // challenge.ts
 const prettyPrintWilder = (users: User[]): void => {
-  users.map((user) => {
+  users.map((user: User) => {
     console.log(`${user.name} is ${user.age} years old`);
   });
 };
 
+export interface User {
+  name: string;
+  age: number;
+  // birthday?: string;
+  // [key: string]: any;
+}
+
 const wilders: User[] = [];
 const user1 = { name: "Pierre", age: 23 };
-const user2 = { name: "Paul", birthday: "10/02/1990" };
+const user2 = { name: "Paul", age: 23, birthday: "10/02/1990" };
 const user3 = { name: "Jacques", age: 25 };
 wilders.push(user1);
 wilders.push(user2);
 wilders.push(user3);
 prettyPrintWilder(wilders);
 
-export interface User {
-  name: string;
-  age?: number;
-  birthday?: string;
-}
+// console.log(prettyPrintWilder(wilders));
